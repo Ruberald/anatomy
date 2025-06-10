ac498ec4535f11397737d08cb5145f8fea40f53f
Ruberald <sajidhossain909@gmail.com>
2025-06-11 04:01:36 +0530

    Hex parser added

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -17,6 +17,23 @@ impl REPL {
         }
     }
 
+    fn parse_hex(&mut self, i: &str) -> Result<Vec<u8>, std::num::ParseIntError>{
+        let split = i.split(" ").collect::<Vec<&str>>();
+        let mut results: Vec<u8> = vec![];
+        for hex_string in split {
+            let byte = u8::from_str_radix(&hex_string, 16);
+            match byte {
+                Ok(result) => {
+                    results.push(result);
+                },
+                Err(e) => {
+                    return Err(e);
+                }
+            }
+        }
+        Ok(results)
+    }
+
     pub fn run(&mut self) {
         const VERSION: &str = env!("CARGO_PKG_VERSION");
         println!("Anatomy REPL {VERSION}");
@@ -47,8 +64,31 @@ impl REPL {
                     }
                 }
 
+                ".program" => {
+                    println!("Listing instructions currently in VM's program vector:");
+                    for instruction in &self.vm.program {
+                        println!("{}", instruction);
+                    }
+                    println!("End of Program Listing");
+                },
+
+                ".registers" => {
+                    println!("Listing registers and all contents:");
+                    println!("{:#?}", self.vm.registers);
+                    println!("End of Register Listing")
+                },
+
                 _ => {
-                    println!("Invalid expression, command or input.");
+                    let results = self.parse_hex(trim_buffer);
+                    match results {
+                        Ok(mut bytes) => {
+                            self.vm.program.append(&mut bytes);
+                        },
+                        Err(_e) => {
+                            println!("Unable to decode hex string. Please enter 4 groups of 2 hex characters.")
+                        }
+                    };
+                    self.vm.run_once();
                 }
             }
 
